850477447ff4548e070843e0dea89445dd973401
doxuandungx10 <52046541+doxuandungx10@users.noreply.github.com>
2023-02-19 14:44:21 +0700

    * remove animate.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
-import 'animate.css'
+// import 'animate.css'
 
 
 
