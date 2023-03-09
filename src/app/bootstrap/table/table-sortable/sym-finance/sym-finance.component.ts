f22b8ea26b67b447d85e8a7dea6d2e4c6781d0da
Do Xuan Dung <dungdx@pmr.vn>
2023-03-09 18:16:05 +0700

    ++ Báo cáo tài chính

--- src/app/bootstrap/table/table-sortable/sym-finance/sym-finance.component.ts
+++ src/app/bootstrap/table/table-sortable/sym-finance/sym-finance.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StockDetailService } from 'src/app/service/general-service/stock-detail.service';
 
 @Component({
   selector: 'app-sym-finance',
   templateUrl: './sym-finance.component.html',
-  styleUrls: ['./sym-finance.component.css']
+  styleUrls: ['./sym-finance.component.css'],
 })
 export class SymFinanceComponent implements OnInit {
+  @Input() sym: any = '';
+  stockFinanceYear: any[] = [];
+  stockFinancePeriod: any[] = [];
 
-  constructor() { }
+  constructor(private stockDetailService: StockDetailService) {}
 
   ngOnInit(): void {
+    this.getBaseInfo();
   }
 
+  getBaseInfo() {
+    this.stockDetailService.getBaseInfo(this.sym).subscribe((res: any) => {
+      this.stockFinanceYear = res.ketquaKDNam;
+      this.stockFinancePeriod = res.ketquaKDQuy;
+      console.log(res);
+    });
+  }
+
+  toCurrencyFormat(data) {
+    return new Intl.NumberFormat().format(data);
+  }
 }
